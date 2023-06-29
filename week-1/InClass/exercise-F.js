c9ca31c5b3393f155726d597e07c77e2171b3832
alpa9029 <alpa9029@gmail.com>
2023-06-29 17:47:53 +0200

    donef

---
+++ week-1/InClass/exercise-F.js
@@ -0,0 +1,6 @@
+console.log("Hello World!")
+var numberOfStudents = 20;
+var numberOfMentors = 7;
+
+var total = numberOfStudents + numberOfMentors;
+console.log("Total number of students and mentors: " + total);
