a401443c51aec13f7a41e70fc94e032027e4334c
alpa9029 <alpa9029@gmail.com>
2023-06-29 18:18:06 +0200

    doneh

--- week-2/InClass/exercise-H.js
+++ week-2/InClass/exercise-H.js
@@ -1,7 +1,13 @@
+console.log("Hello World!")
 function exponential(number) {
   return number * number;
 }
-
 function isEven(number) {
   return number % 2 === 0;
 }
+for (let i = 5; i <= 20; i++) {
+  if (isEven(i)) {
+    console.log(`Exponential of ${i}: ${exponential(i)}`);
+  }
+}
+
