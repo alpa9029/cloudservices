a88c941d7909d936864db4bbc56961a4dfc75723
alpa9029 <alpa9029@gmail.com>
2023-06-29 18:12:39 +0200

    doneb

--- week-2/InClass/exercise-B.js
+++ week-2/InClass/exercise-B.js
@@ -1,10 +1,15 @@
 console.log("Hello World!")
+// let codeYourFutureIsGreat = true;
+let thisIsATerribleClass = false;
+let testType =  typeof thisIsATerribleClass;
+console.log("the type should be boolean, is it really boolean?? >> the answer is: ",testType);
 function boolChecker(bool) {
-  if (typeof bool === ) {
-    return "You've given me a bool, thanks!";
+    if (typeof bool === "boolean" ) {
+      return "You've given me a bool, thanks!";
+    }
+    return "No bool, not cool.";
   }
-
-  return "No bool, not cool.";
-}
-
-boolChecker(true);
+ let message =  boolChecker(true);
+ console.log(message)
+ console.log("Any time!");
+ 
